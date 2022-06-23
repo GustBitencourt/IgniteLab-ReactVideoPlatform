d601f5b8f521fb7118c09fdd5608abbeaf3185e4
GustBitencourt <gubiten@gmail.com>
2022-06-23 16:15:21 -0300

    Card Lessons e Asidebar, estrutura e logica

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,6 +1,31 @@
+import { gql, useQuery } from "@apollo/client";
 import { CardLessons } from "../CardLessons"
 
+const GET_LESSONS_QUERY = gql`
+    query {
+        lessons(orderBy: publishedAt_ASC, stage: PUBLISHED) {
+            id
+            slug
+            title
+            availableAt
+            lessonType
+        }
+    }
+`;
+
+interface GetLessonsQueryResponse {
+    lessons: {
+        id: string;
+        slug: string;
+        title: string;
+        availableAt: string;
+        lessonType: 'live' | 'class';
+    }
+}
+
 export const Sidebar = () => {
+    const { data } = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY);
+
     return (
         <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
             <span className="font-bold text-2xl pb-6 mb-6 border-gray-500 block">
@@ -8,12 +33,17 @@ export const Sidebar = () => {
             </span>
 
             <div className="flex flex-col gap-8">
-                <CardLessons />
-                <CardLessons />
-                <CardLessons />
-                <CardLessons />
-                <CardLessons />
-                <CardLessons />
+                {data?.lessons.map(lesson => {
+                    return (
+                        <CardLessons
+                            key={lesson.id}
+                            title={lesson.title}
+                            slug={lesson.slug}
+                            avaibleAt={new Date(lesson.availableAt)}
+                            type={lesson.lessonType}
+                        />
+                    )
+                })}
             </div>
         </aside>
     )
