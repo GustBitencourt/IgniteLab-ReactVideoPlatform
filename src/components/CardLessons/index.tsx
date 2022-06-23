77d808fbdc377a44bc2a72e4f5a80adaf2afa611
GustBitencourt <gubiten@gmail.com>
2022-06-23 14:55:35 -0300

    Configuracao tailwind, estrutura e estilo header

---
+++ src/components/CardLessons/index.tsx
@@ -0,0 +1,5 @@
+export const CardLessons = () => {
+  return (
+    <div>CardLessons</div>
+  )
+}
---
+++ src/pages/Evento/index.tsx
@@ -0,0 +1,15 @@
+import { CardLessons } from "../../components/CardLessons"
+import { Header } from "../../components/Header"
+import { Sidebar } from "../../components/Sidebar"
+import { Video } from "../../components/Video"
+
+export const Evento = () => {
+    return (
+        <div>
+            <Header />
+            {/* <Sidebar />
+            <Video />
+            <CardLessons /> */}
+        </div>
+    )
+}
