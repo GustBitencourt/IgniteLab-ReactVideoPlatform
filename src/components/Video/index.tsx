6e2cf9d5909560cd20e2da02cdd510bcfc714e17
GustBitencourt <gubiten@gmail.com>
2022-06-24 14:59:20 -0300

    logica do carregamento de diferentes aulas

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -38,11 +38,15 @@ interface VideoProps {
 
 export const Video = (props: VideoProps) => {
     //chamando dados de uma unica aula, através do seu slug, utilizando query acima
-    const { data } = useQuery(GET_LESSONS_BY_SLUG_QUERY, {
+    const { data } = useQuery<GetLessonsBySlugResponse>(GET_LESSONS_BY_SLUG_QUERY, {
         variables: {
             slug: props.lessonSlug
         }
     })
+
+    if (!data) {
+        return <div className="flex-1">Loading...</div>
+    }
 
 
     return (
@@ -50,7 +54,7 @@ export const Video = (props: VideoProps) => {
             <div className="bg-black flex justify-center">
                 <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
                     <Player>
-                        <Youtube videoId="SO4-izct7Mc" />
+                        <Youtube videoId={data.lesson.videoId} />
                         <DefaultUi />
                     </Player>
                 </div>
@@ -62,22 +66,22 @@ export const Video = (props: VideoProps) => {
                     {/* Titulo e descricao e professor */}
                     <div className="flex-1">
                         <h1 className="text-2xl font-bold">
-                            Aula 01 - Abertura Ignite Lab
+                            {data.lesson.title}
                         </h1>
                         <p className="mt-4 text-gray-200 leading-relaxed">
-                            Nessa aula vamos dar início ao projeto criando a estrutura base da aplicação utilizando ReactJS, Vite e TailwindCSS. Vamos também realizar o setup do nosso projeto no GraphCMS criando as entidades da aplicação e integrando a API GraphQL gerada pela plataforma no nosso front-end utilizando Apollo Client.
+                            {data.lesson.description}
                         </p>
 
                         <div className="flex items-center gap-4 mt-6">
                             <img
                                 className="h-16 w-16 rounded-full border-2 border-blue-500"
-                                src="https://github.com/GustBitencourt.png"
-                                alt="Gustavo Bitencourt"
+                                src={data.lesson.teacher.avatarURL}
+                                alt={data.lesson.teacher.name}
                             />
 
                             <div className="leading-relaxed">
-                                <strong className="font-bold text-2xl block">Gustavo Bitencourt</strong>
-                                <span className="text-gray-200 text-sm block">Estudante de Desenvolvimento</span>
+                                <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
+                                <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
                             </div>
                         </div>
                     </div>
